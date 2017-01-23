77c5e0fcd7dbb3ed61a2f7684df1799a4f311710
lokeshreddy <lokeshreddy@lokeshreddys-mac.local>
2017-01-23 11:29:20 -0500

    Initial Commit

---
+++ UserNotfications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserNotfications
+//
+//  Created by lokeshreddy on 1/23/17.
+//  Copyright © 2017 lokeshreddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
