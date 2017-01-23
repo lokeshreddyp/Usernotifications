5af143decd0ddf3cecb0c9d9275564ce0b694369
lokeshreddy <lokeshreddy@lokeshreddys-Mac.local>
2017-01-23 13:02:34 -0500

    getting user notifications locally and adding attachments and content to it

--- UserNotfications/ViewController.swift
+++ UserNotfications/ViewController.swift
@@ -7,19 +7,88 @@
 //
 
 import UIKit
-
+import UserNotifications
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        
+        //request permission from the user
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.badge,.sound], completionHandler: {(granted , error) in
+            
+            if granted {
+             print("Notification access ")
+            }
+            else {
+                print(error?.localizedDescription)
+            }
+        })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    @IBAction func sendnotificationpressed(_ sender: UIButton) {
+        schedulenotification(inSeconds: 5, completion: {
+            sucess in
+            
+            if sucess {
+                print("sucessfully scheduled notification")
+            }
+            else{
+                print("error")
+            }
+        })
+       
     }
-
-
+    
+    
+    func schedulenotification(inSeconds: TimeInterval, completion: @escaping (Bool) -> ()) {
+        
+        
+        //add an attachment
+        
+     let myimage = "Green"
+        guard let imageurl = Bundle.main.url(forResource: "Green", withExtension: "jpg") else {
+            completion(false)
+            return
+        }
+        
+        var attachment: UNNotificationAttachment
+        
+        attachment = try! UNNotificationAttachment(identifier: "myNotification", url: imageurl, options: .none)
+        
+        
+        
+        //add content to notification
+        let notification = UNMutableNotificationContent()
+        notification.title = "New Notification"
+        notification.subtitle = "These are great"
+        notification.body = "The new notification options in ios are great!!!!!"
+        
+        notification.attachments = [attachment]
+ 
+        let notificationtrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "myNotification", content: notification,trigger: notificationtrigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+            
+            
+            if error != nil {
+                print(error)
+                completion(false)
+            }
+            else {
+                completion(true)
+            }
+            
+            
+        })
+    
+    
+    
+    
+    }
+    
 }
 
